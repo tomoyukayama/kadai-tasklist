9ab4b1d132f4b07e33c3c3b05d28ce7aecf00f82
tomoyuki.takayama <ttakayama.ka0723@gmail.com>
2020-07-19 07:40:23 +0000

    CRUD messages

--- database/migrations/2020_07_12_221803_create_tasks_table.php
+++ database/migrations/2020_07_12_221803_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMessagesTable extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -27,6 +27,6 @@ class CreateMessagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('tasks');
     }
 }
